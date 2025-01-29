5e08414a823f8e2d93a3ce2b63cdb746db9ff064
Shuflduf <shuflduf@gmail.com>
2025-01-29 03:47:34 -0700

    enhance search API to support both movies and TV shows, implement pagination, and improve loading state management

--- src/routes/api/search/[id]/+server.ts
+++ src/routes/api/search/[id]/+server.ts
@@ -1,9 +1,12 @@
 import { env } from "$env/dynamic/private";
 import type { RequestHandler } from "@sveltejs/kit";
-import type { movieData } from "../../../../lib/Movie.svelte";
+import type { mediaData } from "$lib/MediaCard.svelte";
 
-export const GET: RequestHandler = async ({ params }) => {
+export const GET: RequestHandler = async ({ params, url }) => {
   const query = params.id;
+  const page = url.searchParams.get("page") || "1";
+  const type = url.searchParams.get("type");
+
   if (!query) {
     return new Response(
       JSON.stringify({ error: "Query parameter is missing" }),
@@ -15,9 +18,62 @@ export const GET: RequestHandler = async ({ params }) => {
       },
     );
   }
-  const response = await fetch(
-    `https://api.themoviedb.org/3/search/movie?query=${query}`,
 
+  if (!type) {
+    const movieResponse = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${query}&page=${page}`,
+      {
+        headers: {
+          Authorization: `Bearer ${env.TMDB_API_KEY}`,
+          accept: "application/json",
+        },
+      },
+    );
+
+    const showResponse = await fetch(
+      `https://api.themoviedb.org/3/search/tv?query=${query}&page=${page}`,
+      {
+        headers: {
+          Authorization: `Bearer ${env.TMDB_API_KEY}`,
+          accept: "application/json",
+        },
+      },
+    );
+
+    if (!movieResponse.ok || !showResponse.ok) {
+      return new Response(JSON.stringify({ error: "Failed to fetch data" }), {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
+
+    const movieData = await movieResponse.json();
+    const showData = await showResponse.json();
+
+    const movies: mediaData[] = movieData.results;
+    const shows: mediaData[] = showData.results;
+
+    return new Response(JSON.stringify({ movies, shows }), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  if (type !== "movie" && type !== "tv") {
+    return new Response(JSON.stringify({ error: "Invalid type parameter" }), {
+      status: 400,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/${type}?query=${query}&page=${page}`,
     {
       headers: {
         Authorization: `Bearer ${env.TMDB_API_KEY}`,
@@ -25,10 +81,20 @@ export const GET: RequestHandler = async ({ params }) => {
       },
     },
   );
-  const data = await response.json();
-  const movies: movieData[] = data.results;
 
-  return new Response(JSON.stringify(movies), {
+  if (!response.ok) {
+    return new Response(JSON.stringify({ error: "Failed to fetch data" }), {
+      status: response.status,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  const data = await response.json();
+  const media: mediaData[] = data.results;
+
+  return new Response(JSON.stringify(media), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
--- src/routes/search/[id]/+page.svelte
+++ src/routes/search/[id]/+page.svelte
@@ -1,21 +1,117 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  import { onMount, afterUpdate } from "svelte";
   import { page } from "$app/stores";
-  import type { movieData } from "$lib/Movie.svelte";
-  import Movie from "$lib/Movie.svelte";
+  import type { mediaData } from "$lib/MediaCard.svelte";
+  import MediaRow from "$lib/MediaRow.svelte";
+  import MediaCard from "$lib/MediaCard.svelte";
   import SearchBar from "$lib/SearchBar.svelte";
+  import SkeletonCard from "$lib/SkeletonCard.svelte";
 
-  let movies: movieData[] = [];
-  let isLoading = true;
+  let movies: mediaData[] = [];
+  let shows: mediaData[] = [];
+  let mediaItems: mediaData[] = [];
+  let isLoading = false;
   let query = $page.params.id;
+  let currentPage = 1;
+  let hasMore = true;
+  let type = $page.url.searchParams.get("type");
 
-  onMount(async () => {
+  async function loadMedia(type: string) {
+    if (isLoading || !hasMore) return;
+
+    isLoading = true;
     const response = await fetch(
-      `/api/search/${encodeURIComponent($page.params.id)}`,
+      `/api/search/${encodeURIComponent(query)}?type=${type}&page=${currentPage}`,
     );
-    const data = await response.json();
-    movies = data;
+    if (response.ok) {
+      const newItems = await response.json();
+      if (newItems.length === 0) {
+        hasMore = false;
+      } else {
+        if (type === "movie") {
+          movies = [...movies, ...newItems];
+        } else {
+          shows = [...shows, ...newItems];
+        }
+        currentPage += 1;
+      }
+    } else {
+      console.error("Failed to fetch media items");
+    }
     isLoading = false;
+  }
+
+  async function loadAllMedia() {
+    if (isLoading || !hasMore) return;
+
+    isLoading = true;
+    const response = await fetch(
+      `/api/search/${encodeURIComponent(query)}?type=${type}&page=${currentPage}`,
+    );
+    if (response.ok) {
+      const newItems = await response.json();
+      if (newItems.length === 0) {
+        hasMore = false;
+      } else {
+        mediaItems = [...mediaItems, ...newItems];
+        currentPage += 1;
+      }
+    } else {
+      console.error("Failed to fetch media items");
+    }
+    isLoading = false;
+  }
+
+  function handleScroll() {
+    const bottom =
+      window.innerHeight + window.scrollY >= document.body.offsetHeight - 100;
+    if (bottom) {
+      if (!type) {
+        loadMedia("movie");
+        loadMedia("tv");
+      } else {
+        loadAllMedia();
+      }
+    }
+  }
+
+  function resetState() {
+    movies = [];
+    shows = [];
+    mediaItems = [];
+    isLoading = false;
+    currentPage = 1;
+    hasMore = true;
+  }
+
+  async function initialize() {
+    resetState();
+    if (!type) {
+      await loadMedia("movie");
+      await loadMedia("tv");
+    } else {
+      await loadAllMedia();
+    }
+  }
+
+  onMount(() => {
+    initialize();
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  afterUpdate(() => {
+    const newQuery = $page.params.id;
+    const newType = $page.url.searchParams.get("type");
+
+    if (newQuery !== query || newType !== type) {
+      query = newQuery;
+      type = newType;
+      initialize();
+    }
   });
 </script>
 
@@ -23,29 +119,34 @@
   <SearchBar />
   <h1 class="text-2xl font-bold mb-4 px-4">Search Results for "{query}"</h1>
   <div class="px-4">
-    <div
-      class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4"
-    >
-      {#if isLoading}
-        {#each Array(12) as _}
-          <div class="w-full">
-            <div class="rounded-2xl overflow-hidden">
-              <div class="w-full h-[225px] bg-gray-700 animate-pulse relative">
-                <div
-                  class="absolute bottom-3 left-3 right-3 h-6 bg-gray-600 rounded-full"
-                ></div>
-              </div>
-            </div>
-          </div>
-        {/each}
-      {:else}
-        {#each movies as movie}
-          <div class="w-full">
-            <Movie data={movie} />
-          </div>
-        {/each}
-      {/if}
-    </div>
+    {#if !type}
+      <MediaRow
+        title="Movies"
+        link={`/search/${query}?type=movie`}
+        mediaItems={movies}
+        {isLoading}
+      />
+      <MediaRow
+        title="Shows"
+        link={`/search/${query}?type=tv`}
+        mediaItems={shows}
+        {isLoading}
+      />
+    {:else}
+      <div
+        class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4"
+      >
+        {#if isLoading && mediaItems.length === 0}
+          {#each Array(12) as _}
+            <SkeletonCard className="w-full" />
+          {/each}
+        {:else}
+          {#each mediaItems as media}
+            <MediaCard data={media} />
+          {/each}
+        {/if}
+      </div>
+    {/if}
   </div>
 </main>
 
