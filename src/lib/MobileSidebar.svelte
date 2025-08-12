c3b6bd5dc5c72392e56df91769f540b8ad033b39
Shuflduf <shuflduf@shuflduf.xyz>
2025-08-12 09:44:53 -0600

    AAAAAAAAAA

--- src/lib/MobileSidebar.svelte
+++ src/lib/MobileSidebar.svelte
@@ -18,13 +18,13 @@
   class="absolute w-[calc(100vw-2rem)] md:relative {mobileOpen ? 'h-full' : ''}"
 >
   <button
-    class="absolute top-4 right-4 z-30 block h-12 w-12 rounded-md bg-blue-400 text-white md:hidden"
+    class="absolute top-4 right-4 z-30 block rounded-md bg-blue-400 px-4 py-2 font-[Arvo] text-xl font-bold text-white md:hidden"
     onclick={() => (mobileOpen = !mobileOpen)}
   >
     {#if mobileOpen}
-      &gt
+      Hide Menu
     {:else}
-      &lt
+      Show Menu
     {/if}
   </button>
 
