4a80f92cf67179b69efd239bf773987d6a026127
Shuflduf <shuflduf@shuflduf.xyz>
2025-08-12 08:19:23 -0600

    main page is now fully* usable on mobile

--- src/routes/ContinueList.svelte
+++ src/routes/ContinueList.svelte
@@ -7,9 +7,15 @@
   import { fly } from "svelte/transition";
 
   let continueMedia: Media[] = $state([]);
+  let mobileOpen: boolean = $state(false);
 
   onMount(() => {
     continueMedia = lastWatched();
+    mobileOpen = window.innerWidth > 768;
+    window.addEventListener(
+      "resize",
+      (_) => (mobileOpen = window.innerWidth > 768),
+    );
   });
 
   function removeTop() {
@@ -18,35 +24,51 @@
   }
 </script>
 
-<div
-  class="mx-4 max-h-[calc(100vh-7.5rem)] w-[calc(100vw-2rem)] md:mx-0 md:w-full {BOX_STYLE} absolute left-0 flex-col overflow-y-auto backdrop-blur-md md:relative md:flex"
-  in:fly|global={{ y: 100, duration: 300, delay: 100 }}
->
-  <h1 class="mb-4 font-[Arvo] text-2xl dark:text-white">Continue</h1>
-  {#if continueMedia.length > 0}
-    <button
-      class="mb-4 w-full cursor-pointer rounded-md bg-red-400 p-2 font-[Arvo] font-bold text-white transition hover:bg-red-500"
-      onclick={removeTop}
+<div class="absolute w-[calc(100vw-2rem)] md:relative">
+  <button
+    class="absolute top-4 right-4 z-30 block h-12 w-12 rounded-md bg-blue-400 text-white md:hidden"
+    onclick={() => (mobileOpen = !mobileOpen)}
+  >
+    {#if mobileOpen}
+      &lt
+    {:else}
+      &gt
+    {/if}
+  </button>
+
+  {#if mobileOpen}
+    <div
+      class="max-h-[calc(100vh-7.5rem)] w-[calc(100vw-2rem)] md:mx-0 md:w-full {BOX_STYLE} absolute left-0 flex-col overflow-y-auto backdrop-blur-md transition md:relative md:flex"
+      in:fly|global={{ y: 100, duration: 300, delay: 100 }}
     >
-      Remove Top
-    </button>
-    <div class="flex flex-col gap-4">
-      {#each continueMedia as content, index (content)}
-        <div
-          class={BOX_STYLE}
-          in:fly|global={{
-            y: 100,
-            duration: 300,
-            delay: 100 + index * 100,
-          }}
+      <h1 class="mb-4 font-[Arvo] text-2xl dark:text-white">Continue</h1>
+
+      {#if continueMedia.length > 0}
+        <button
+          class="mb-4 w-full cursor-pointer rounded-md bg-red-400 p-2 font-[Arvo] font-bold text-white transition hover:bg-red-500"
+          onclick={removeTop}
         >
-          <Overview {content} />
+          Remove Top
+        </button>
+        <div class="flex flex-col gap-4">
+          {#each continueMedia as content, index (content)}
+            <div
+              class={BOX_STYLE}
+              in:fly|global={{
+                y: 100,
+                duration: 300,
+                delay: 100 + index * 100,
+              }}
+            >
+              <Overview {content} />
+            </div>
+          {/each}
         </div>
-      {/each}
-    </div>
-  {:else}
-    <div class="flex h-full items-center justify-center">
-      <p class="text-slate-500">Your continue watching list is empty.</p>
+      {:else}
+        <div class="flex h-full items-center justify-center">
+          <p class="text-slate-500">Your continue watching list is empty.</p>
+        </div>
+      {/if}
     </div>
   {/if}
 </div>
